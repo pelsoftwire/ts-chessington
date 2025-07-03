2f55fc725f66f3dbcaf6d4c42a69de2c4acce54a
Pelumi Babalola <Pelumi.Babalola@softwire.com>
2025-07-03 11:55:04 +0100

    added knight movement

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import gameSettings from "../gameSettings";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,23 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        var currentPosition : Square = board.findPiece(this);
+        var availableMoves : Square[] = [];
+
+        var cardinals: [number, number][] = [[2, 1], [1, 2]];
+        var masks: [number, number][] = [[1, 1], [1, -1], [-1, -1], [-1, 1]];
+
+        for (const [xa, ya]  of cardinals) {
+            for (const [dx, dy] of masks) {
+                var x = currentPosition.row + xa * dx;
+                var y = currentPosition.col + ya * dy;
+                if (0 <= x && x < gameSettings.BOARD_SIZE && 0 <= y && y < gameSettings.BOARD_SIZE) {
+                    availableMoves.push(new Square(x, y));
+                }
+            }
+        }
+
+        return availableMoves;
+
     }
 }
