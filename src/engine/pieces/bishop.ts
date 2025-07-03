59c1be50802996e6289947ce30c611e992e9794d
Pelumi Babalola <Pelumi.Babalola@softwire.com>
2025-07-03 11:38:26 +0100

    bishops can move diagonally

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import gameSettings from "../gameSettings";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,21 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        var availableMoves : Square[] = [];
+        var currentSquare : Square = board.findPiece(this);
+
+        var cardinals : [number, number][] = [[1, 1], [1, -1], [-1, 1], [-1, -1]];
+
+        for (const [xp, yp] of cardinals) {
+            var x = currentSquare.row + xp;
+            var y = currentSquare.col + yp;
+            while (0 <= x && x < gameSettings.BOARD_SIZE && 0 <= y && y < gameSettings.BOARD_SIZE) {
+                availableMoves.push(new Square(x, y));
+                x += xp;
+                y += yp;
+            }
+        }
+
+        return availableMoves;
     }
 }
