a92daeceb63e70d8e7b52de324851bb43e66cccf
Pelumi Babalola <Pelumi.Babalola@softwire.com>
2025-07-03 12:19:54 +0100

    slidingMovement no longer allows movement through pieces

--- src/engine/pieces/slidingMovement.ts
+++ src/engine/pieces/slidingMovement.ts
@@ -11,10 +11,16 @@ export default function slidingMovement(board: Board, cardinals: [number, number
     for (const [xp, yp] of cardinals) {
         var x = currentSquare.row + xp;
         var y = currentSquare.col + yp;
-        while (0 <= x && x < gameSettings.BOARD_SIZE && 0 <= y && y < gameSettings.BOARD_SIZE) {
-            availableMoves.push(new Square(x, y));
+        var toMove : Square = new Square(x, y);
+        while (0 <= x && x < gameSettings.BOARD_SIZE && 0 <= y && y < gameSettings.BOARD_SIZE && board.whatsAt(toMove) == undefined) {
+            availableMoves.push(toMove);
             x += xp;
             y += yp;
+            toMove = new Square(x, y);
+
+            // TODO: Figure out why this doesnt work
+            //toMove.row += xp;
+            //toMove.col += yp;
         }
     }
 
