b805ba3acf3e453361495628ca0f99dadc9eb682
Pelumi Babalola <Pelumi.Babalola@softwire.com>
2025-07-03 13:53:55 +0100

    knight cannot take allied pieces or the enemy king

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -3,6 +3,7 @@ import Player from '../player';
 import Board from '../board';
 import Square from "../square";
 import gameSettings from "../gameSettings";
+import King from "./king";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -21,7 +22,13 @@ export default class Knight extends Piece {
                 var x = currentPosition.row + xa * dx;
                 var y = currentPosition.col + ya * dy;
                 if (0 <= x && x < gameSettings.BOARD_SIZE && 0 <= y && y < gameSettings.BOARD_SIZE) {
-                    availableMoves.push(new Square(x, y));
+                    var toMove : Square = new Square(x, y);
+                    var pieceAtSquare : Piece | undefined = board.whatsAt(toMove)
+
+                    // allow movement if space is empty, or if space contains an enemy piece that is NOT the king (taking a piece)
+                    if (pieceAtSquare == undefined || (pieceAtSquare.player != this.player && ! (pieceAtSquare instanceof King))) {
+                        availableMoves.push(new Square(x, y));
+                    }
                 }
             }
         }
