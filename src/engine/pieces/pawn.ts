a8d57f2bf59c080db08ec6ab229f097f1e79eae1
Pelumi Babalola <Pelumi.Babalola@softwire.com>
2025-07-03 09:53:52 +0100

    pawns can move one space (for real this time)

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,12 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        var currentSquare : Square = board.findPiece(this);
+        if (this.player == Player.WHITE) {
+            currentSquare.row = currentSquare.row + 1;
+        } else {
+            currentSquare.row = currentSquare.row - 1;
+        }
+        return [currentSquare];
     }
 }
