12464e5279227179532468a0ddb94bda6bf8999d
Pelumi Babalola <Pelumi.Babalola@softwire.com>
2025-07-03 10:12:04 +0100

    rooks can move laterally across the board

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import gameSettings from "../gameSettings";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,22 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        var availableMoves : Square[] = [];
+        var currentSquare : Square = board.findPiece(this);
+
+        for (var i = currentSquare.row + 1; i < gameSettings.BOARD_SIZE; ++i) {
+            availableMoves.push(new Square(i, currentSquare.col));
+        }
+        for (var i = currentSquare.row - 1; i >= 0; --i) {
+            availableMoves.push(new Square(i, currentSquare.col));
+        }
+        for (var i = currentSquare.col + 1; i < gameSettings.BOARD_SIZE; ++i) {
+            availableMoves.push(new Square(currentSquare.row, i));
+        }
+        for (var i = currentSquare.col - 1; i >= 0; --i) {
+            availableMoves.push(new Square(currentSquare.row, i));
+        }
+
+        return availableMoves;
     }
 }
