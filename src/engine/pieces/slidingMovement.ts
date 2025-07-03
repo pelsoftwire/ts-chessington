959721e997f877791e5545de956e673442b284ba
Pelumi Babalola <Pelumi.Babalola@softwire.com>
2025-07-03 13:47:40 +0100

    slidingMovement allows taking of opposing pieces

--- src/engine/pieces/slidingMovement.ts
+++ src/engine/pieces/slidingMovement.ts
@@ -2,6 +2,7 @@ import Square from "../square";
 import Board from "../board";
 import gameSettings from "../gameSettings";
 import Piece from "./piece";
+import King from "./king";
 
 export default function slidingMovement(board: Board, cardinals: [number, number][], piece: Piece): Square[] {
 
@@ -12,11 +13,23 @@ export default function slidingMovement(board: Board, cardinals: [number, number
         var x = currentSquare.row + xp;
         var y = currentSquare.col + yp;
         var toMove : Square = new Square(x, y);
-        while (0 <= x && x < gameSettings.BOARD_SIZE && 0 <= y && y < gameSettings.BOARD_SIZE && board.whatsAt(toMove) == undefined) {
-            availableMoves.push(toMove);
-            x += xp;
-            y += yp;
-            toMove = new Square(x, y);
+        while (0 <= x && x < gameSettings.BOARD_SIZE && 0 <= y && y < gameSettings.BOARD_SIZE) {
+            var pieceAtSquare : Piece | undefined  = board.whatsAt(toMove);
+            // TODO: add rook can take opposing pieces ONLY
+            if (pieceAtSquare == undefined) {
+                availableMoves.push(toMove);
+                x += xp;
+                y += yp;
+                toMove = new Square(x, y);
+            } else if (pieceAtSquare.player != piece.player && ! (pieceAtSquare instanceof King)) {
+                availableMoves.push(toMove);
+                x += xp;
+                y += yp;
+                toMove = new Square(x, y);
+                break;
+            } else {
+                break;
+            }
 
             // TODO: Figure out why this doesnt work
             //toMove.row += xp;
